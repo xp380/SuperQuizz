dbaa514d18e977fca0e43009c37717adc06a78c1
Vincent Kouoï <vincentkouoi@gmail.com>
2022-03-11 16:00:59 +0100

    head and footer set up

--- front/src/HomePage.js
+++ front/src/HomePage.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./HomePage.css";
 
-const HomePage = () => {
-  return <div></div>;
+import { Layout } from "antd";
+const { Header, Footer, Content } = Layout;
+
+const foot = {
+  borderTop: "1px solid #e8e8e8",
+  position: "fixed",
+  left: 0,
+  bottom: 0,
+  width: "100%",
+  backgroundColor: "#001529",
+  color: "white",
+  height: 50,
 };
 
-export default HomePage;
+export default function HomePage() {
+  return (
+    <div>
+      <Layout>
+        <Header className="head">Test</Header>
+        <Content className="content">Contenu</Content>
+      </Layout>
+
+      <Footer style={foot}>Test</Footer>
+    </div>
+  );
+}
