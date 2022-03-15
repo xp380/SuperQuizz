d7a5f8923426da02f30a7fe0b1e58025529c6ee8
Vincent Kouoï <vincentkouoi@gmail.com>
2022-03-15 14:49:43 +0100

    first commit

--- front/src/HomePage.js
+++ front/src/HomePage.js
@@ -6,7 +6,7 @@ import Time from "./Components/Time";
 import Contact from "./Components/Contact";
 import "./HomePage.css";
 import { Layout, Tabs } from "antd";
-const { Header, Footer } = Layout;
+const { Header, Footer, Content } = Layout;
 
 const { TabPane } = Tabs;
 
@@ -18,31 +18,33 @@ export default function HomePage() {
     <div>
       <Layout>
         <Header className="head">Test0</Header>
-        <button
-          style={{
-            width: 200,
-            height: 30,
-            backgroundColor: "black",
-            color: "white",
-          }}
-          onClick={refreshPage}
-        >
-          Click to replay!
-        </button>
-        <Tabs defaultActiveKey="1">
-          <TabPane tab="Code" key="1">
-            <Code />
-          </TabPane>
-          <TabPane tab="History" key="2">
-            <History />
-          </TabPane>
-          <TabPane tab="Joke" key="3">
-            <Joke />
-          </TabPane>
-          <TabPane tab="Contact" key="4">
-            <Contact />
-          </TabPane>
-        </Tabs>
+        <Content className="layout-content">
+          <button
+            style={{
+              width: 200,
+              height: 30,
+              backgroundColor: "black",
+              color: "white",
+            }}
+            onClick={refreshPage}
+          >
+            Click to replay!
+          </button>
+          <Tabs defaultActiveKey="1">
+            <TabPane tab="Code" key="1">
+              <Code />
+            </TabPane>
+            <TabPane tab="History" key="2">
+              <History />
+            </TabPane>
+            <TabPane tab="Joke" key="3">
+              <Joke />
+            </TabPane>
+            <TabPane tab="Contact" key="4">
+              <Contact />
+            </TabPane>
+          </Tabs>
+        </Content>
       </Layout>
       <Footer className="foot">
         <Time />
